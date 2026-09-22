5010fa6c60b639f908a63c6b21ec98b1d6d509a6
linarozo75-gif <linarozo75@gmail.com>
2026-09-21 19:12:14 -0500

    Update page.js

--- app/mi-empresa/experiencia/page.js
+++ app/mi-empresa/experiencia/page.js
@@ -363,6 +363,7 @@ export default function ExperienciaPage() {
         <a href="/mi-empresa" style={estilos.tab}>Mi Empresa</a>
         <a href="/mi-empresa/experiencia" style={{ ...estilos.tab, ...estilos.tabActiva }}>Experiencia</a>
         <a href="/mi-empresa/talento" style={estilos.tab}>Talento</a>
+        <a href="/mi-empresa/financiero" style={estilos.tab}>Financiero</a>
       </nav>
 
       <h1 style={estilos.titulo}>Experiencia</h1>
